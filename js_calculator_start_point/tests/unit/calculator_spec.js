5ec4122a25aec2fe42e49f9676dbd7528cc2d764
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-22 15:55:58 +0100

    add, subtract and multiply unit tests passing

---
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -0,0 +1,33 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  });
+
+  it('should be able to add', function(){
+    calculator.previousTotal = 5
+    calculator.add(3);
+    assert.equal(calculator.runningTotal, 8)
+  });
+
+  it('should be able to subtract', function(){
+    calculator.previousTotal = 6
+    calculator.subtract(2);
+    assert.equal(calculator.runningTotal, 4)
+  });
+
+  it('should be able to multiply', function(){
+    calculator.previousTotal = 3
+    calculator.multiply(4);
+    assert.equal(calculator.runningTotal, 12);
+  })
+
+
+});
