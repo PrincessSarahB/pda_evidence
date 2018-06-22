7ef661968deb8eafd9117ca65cfa33f50409f648
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-22 16:45:10 +0100

    can concatenate multiple number clicks automated test passing

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -53,4 +53,19 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('3')
   });
 
+  it('should be able to concatenate multiple number button clicks', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#number9')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1203456789')
+  });
+
 });
