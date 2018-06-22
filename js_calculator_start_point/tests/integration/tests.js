22d1585d3dd9095b70f02cb4594e6df1c016b736
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-22 16:54:39 +0100

    clear button working, automated test passing

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -83,4 +83,15 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('4')
   });
 
+  it('should be able to clear the running total without affecting the calculation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#clear')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  })
+
 });
