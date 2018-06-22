d284893b20027251b0af0ae0ad432052b9ea2247
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-22 16:50:10 +0100

    can chain multiple button clicks together, automated test passing

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -68,4 +68,19 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('1203456789')
   });
 
+  it('should be able to chain multiple button clicks together', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  });
+
 });
