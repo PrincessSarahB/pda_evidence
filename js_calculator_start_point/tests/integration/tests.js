be92de5891bc0e6c68d9adabbcf9d85c27bd87df
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-23 18:25:31 +0100

    renamed last test

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -94,7 +94,7 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('6')
   });
 
-  it('should return Error when dividing by 0', function(){
+  it('should return Not a Number when dividing by 0', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number4')).click();
     element(by.css('#operator_divide')).click();
