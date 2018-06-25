925df179cc25d13e9b1ae11fc4b66b29fe2773f5
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-25 09:43:09 +0100

    complete

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -95,7 +95,7 @@ describe('calculator functionality', function() {
   });
 
   // original function returns infinity on calculator, function changed to return NaN instead.
-  it('should return Not a Number when dividing by 0', function(){
+  it('should return Error when dividing by 0', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number4')).click();
     element(by.css('#operator_divide')).click();
